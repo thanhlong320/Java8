17bb17873e804feaaa155faeb46dcab062c37791
ntlong <long.nguyenthanh@axonactive.com>
2022-02-25 15:36:32 +0700

    Revert "Create constant and add lombok"
    This reverts commit 6ef6efe4945f487c38059597bccbcaef6ef101b6.

--- src/main/java/constant/Constant.java
+++
@@ -1,9 +0,0 @@
-package constant;
-
-import java.util.Arrays;
-import java.util.List;
-
-public class Constant {
-    public static final List<String> NAMES = Arrays.asList("Nguyen Thanh Long", "Nguyen Phuoc Khanh",
-            "Phung Minh Dat", "Ta Anh Kiet", "Nguyen Van Viet","Hoang Van Hung");
-}
