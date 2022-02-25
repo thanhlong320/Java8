34dd888db11a51003871da17734c82580ef8d2d3
ntlong <long.nguyenthanh@axonactive.com>
2022-02-25 14:05:33 +0700

    Stream API

---
+++ src/main/java/stream_api/model/Classroom.java
@@ -0,0 +1,13 @@
+package stream_api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class Classroom {
+    private int id;
+    private String name;
+}
